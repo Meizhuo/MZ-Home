e833a4c3aa9333c5959179741ed1f2c0efe90eb7
Jayin Ton <tonjayin@gmail.com>
2014-10-14 13:56:04 +0800

    simple

--- p/home.md
+++ p/home.md
@@ -1,1 +1,20 @@
-home.md
+slientor
+===
+本项目受启发与林亮同学的[slient](https://github.com/fritx/silent)
+
+Features
+===
+* markdown only .让你专注文章本身
+* 无需后台,按照指定的地方写markdown就行了
+* Github CSS
+
+```Python
+author = {
+    "name" : "Jayin",
+    "data" : "2014-10-14"
+}
+```
+
+~|~
+---|---
+1 |2
--- vendor/blog.js
+++ vendor/blog.js
@@ -1,2 +1,20 @@
+(function($) {
+    // $('#sidebar-page').load('/p/sidebar.md',function(data){
+    //     console.log(data);
+    // });
 
-console.log("message");
+    function init() {
+        $.get('/mz-home/p/sidebar.md', function(data) {
+
+            $('#sidebar-page').html(marked(data));
+        });
+
+        $.get('/mz-home/p/home.md', function(data) {
+
+            $('#main-page').html(marked(data));
+        });
+    }
+
+    init();
+
+})(jQuery);
